48b60e2a19f4a539b600dbd70120f5ee513b7e43
Matthias Du-Tré <matthiasdutre@msn.com>
2026-02-09 19:31:16 +0100

    more troubleshooting

--- WordFilterer.Core/Storage/Storage.cs
+++ WordFilterer.Core/Storage/Storage.cs
@@ -9,8 +9,8 @@ namespace WordFilterer.Core.Storage;
 
 public class Storage : IStorage
 {
-    public const string inputDirectoryPath = @"..\Storage\Data\Input\";
-    public const string outputDirectoryPath = @"..\Storage\Data\Output\";
+    public const string inputDirectoryPath = @"..\..\..\Data\Input";
+    public const string outputDirectoryPath = @"../Data/Output";
 
     public string firstfile;
 
