1b77f391e619a7ff281dc108e7e005c19d97168e
Matthias Du-Tré <matthiasdutre@msn.com>
2026-02-13 16:39:41 +0100

    final cleanup

--- WordFilterer.Core/Domain/Word.cs
+++ WordFilterer.Core/Domain/Word.cs
@@ -1,18 +1,11 @@
 ﻿namespace WordFilterer.Core.Domain;
 
-public record Word
+// this could be a record if refactoring of combination finding makes the ID check obsolete
+public class Word
 {
     public Guid Id { get; private set; } = Guid.NewGuid();
     public string Content { get; private set; } = string.Empty;
     public int Length => Content.Length;
-
-    //public Word(string content)
-    //{
-    //    Id = Guid.NewGuid();
-    //    Content = content;
-    //}
-
-
     public static Word StringToWord(string content) => new Word() { Content = content ?? String.Empty };
     public static string WordToString(Word word) => word.Content;
 }
